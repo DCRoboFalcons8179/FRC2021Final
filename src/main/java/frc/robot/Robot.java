f277ea3d07a992316678a6d32de575a3958c89a4
ejakovm21 <ejakovm21@divinechildhighschool.org>
2021-02-20 10:38:22 -0500

    remove unused imports

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -8,59 +8,7 @@
 package frc.robot;
 
 
-import edu.wpi.first.wpilibj.kinematics.ChassisSpeeds;
-import edu.wpi.first.wpilibj.ADXRS450_Gyro;
 import edu.wpi.first.wpilibj.TimedRobot;
-import edu.wpi.first.wpilibj.Timer;
-import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
-import edu.wpi.first.cameraserver.CameraServer;
-import edu.wpi.first.networktables.NetworkTableEntry;
-import edu.wpi.cscore.UsbCamera;
-import edu.wpi.cscore.VideoSink;
-import edu.wpi.cscore.VideoMode.PixelFormat;
-import edu.wpi.first.wpilibj.drive.DifferentialDrive;
-import edu.wpi.first.wpilibj.interfaces.Gyro;
-import edu.wpi.first.wpilibj.geometry.*;
-import edu.wpi.first.wpilibj.kinematics.*;
-//import edu.wpi.first.wpilibj.shuffleboard.Shuffleboard;
-import edu.wpi.first.wpilibj.AnalogInput;
-import edu.wpi.first.wpilibj.Joystick;
-import edu.wpi.first.wpilibj.SPI;
-// import edu.wpi.first.wpilibj.GenericHID.Hand;
-// import edu.wpi.first.wpilibj.smartdashboard.SendableChooser;
-// import edu.wpi.first.wpilibj.smartdashboard.*;
-// import edu.wpi.first.wpilibj.PowerDistributionPanel;
-// import edu.wpi.first.wpilibj.SpeedControllerGroup;
-// import com.ctre.phoenix.motorcontrol.can.*;
-// import edu.wpi.first.wpilibj.PWMVictorSPX;
-// import edu.wpi.cscore.VideoMode.PixelFormat;
-// import edu.wpi.first.networktables.*;
-//import edu.wpi.first.wpilibj.Sendable;
-
-import com.ctre.phoenix.motorcontrol.StatusFrameEnhanced;
-//import com.ctre.phoenix.motorcontrol.TalonFXFeedbackDevice;
-import com.ctre.phoenix.motorcontrol.can.WPI_TalonFX;
-import com.ctre.phoenix.motorcontrol.can.TalonFXConfiguration;
-import com.ctre.phoenix.motorcontrol.FeedbackDevice;
-import com.ctre.phoenix.motorcontrol.ControlMode;
-import com.ctre.phoenix.motorcontrol.LimitSwitchNormal;
-import com.ctre.phoenix.motorcontrol.LimitSwitchSource;
-import com.ctre.phoenix.motorcontrol.NeutralMode;
-import com.ctre.phoenix.motorcontrol.can.WPI_VictorSPX;
-import com.ctre.phoenix.motorcontrol.can.WPI_TalonSRX;
-// import com.ctre.phoenix.motorcontrol.Faults;
-// import com.ctre.phoenix.motorcontrol.InvertType;
-// import com.ctre.phoenix.motorcontrol.RemoteSensorSource;
-// import com.ctre.phoenix.motorcontrol.SensorTerm;
-// import com.ctre.phoenix.motorcontrol.FollowerType;
-// import com.ctre.phoenix.motorcontrol.DemandType;
-// import com.ctre.phoenix.motorcontrol.StatusFrame;
-// import com.ctre.phoenix.motorcontrol.TalonFXInvertType;
-// import com.ctre.phoenix.motorcontrol.can.*;
-// import edu.wpi.first.wpilibj.DutyCycleEncoder;
-// import com.ctre.phoenix.motorcontrol.IMotorController;
-// import com.ctre.phoenix.motorcontrol.can.BaseMotorController;
-// import com.ctre.phoenix.motorcontrol.can.VictorSPXPIDSetConfiguration;
 
 public class Robot extends TimedRobot {
 
