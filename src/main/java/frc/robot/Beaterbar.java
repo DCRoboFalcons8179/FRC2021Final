f20036787bbaa1b7acd36368430932f0dcf03ac0
Laptop2 <dcrobotics@gmail.com>
2021-02-27 10:50:53 -0500

    Beater Bar output set

--- src/main/java/frc/robot/Beaterbar.java
+++ src/main/java/frc/robot/Beaterbar.java
@@ -1,5 +1,8 @@
 package frc.robot;
 
+import com.ctre.phoenix.motorcontrol.ControlMode;
+import com.ctre.phoenix.motorcontrol.can.TalonFX;
+
 import edu.wpi.first.wpilibj.Joystick;
 // Inputs
 //need joystick to control beater bar
@@ -12,9 +15,13 @@ public class Beaterbar {
 
     int reverse;
 
+    TalonFX bbar;
+
     //joystick objects are not directly available in main method, get values from controls class
-    public Beaterbar (Joystick inputJoy, int inputForward, int inputReverse) {
+    public Beaterbar (Joystick inputJoy, int inputForward, int inputReverse, TalonFX bbarMotor) {
     
+      bbar = bbarMotor;
+
       joy = inputJoy;
 
       forward = inputForward;
@@ -27,8 +34,16 @@ public class Beaterbar {
     public void periodicbar () {
         //Find if button for Beater Bar is pressed
         if (joy.getRawButton(forward)) {
+
+          bbar.set(ControlMode.PercentOutput, .50);
    
         }  else if (joy.getRawButton(reverse)) {
+
+          bbar.set(ControlMode.PercentOutput, -.50);
+
+        } else {
+
+          bbar.set(ControlMode.PercentOutput, 0); 
 
         }
     }
